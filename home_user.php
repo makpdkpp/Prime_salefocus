619702059823f7f3395be6c97da0691b058aa3d7
Flukspk <flukspk14@gmail.com>
2025-06-24 16:25:08 +0700

    Update home_user.php

--- home_user.php
+++ home_user.php
@@ -218,7 +218,7 @@ $email  = htmlspecialchars($_SESSION['email'], ENT_QUOTES, 'UTF-8');
         const values = rows.map(r => +r.sum_value);
         const total = values.reduce((acc, v) => acc + v, 0);
         
-        const palette = ['rgba(54,162,235,0.8)', 'rgba(255,99,132,0.8)', 'rgba(255,206,86,0.8)', 'rgba(75,192,192,0.8)', 'rgba(153,102,255,0.8)', 'rgba(255,159,64,0.8)'];
+        const palette = ['rgba(54,162,235,0.8)', 'rgba(255,99,132,0.8)', 'rgba(255,206,86,0.8)', 'rgba(75,192,192,0.8)', 'rgba(153,102,255,0.8)', 'rgba(255,159,64,0.8)','rgba(0,128,128,0.8)' ];
         const backgroundColors = values.map((_, i) => palette[i % palette.length]);
         const borderColors = backgroundColors.map(color => color.replace('0.8', '1'));
 
