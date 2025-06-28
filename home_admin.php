f14a9a7d2800c36c9153ac9ec5a3aa90e8ac8fdc
Flukspk <flukspk14@gmail.com>
2025-06-28 14:05:03 +0700

    Update home_admin.php

--- home_admin.php
+++ home_admin.php
@@ -91,15 +91,17 @@ $email = htmlspecialchars($_SESSION['email']);
     </a>
 
     <div class="sidebar">
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
-        <div class="image">
-          <img src="dist_v3/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" style="width: 45px; height: 45px;">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block"><?php echo htmlspecialchars($_SESSION['email'] ?? ''); ?></a>
-          <a href="#" class="d-block" style="color: #c2c7d0; font-size: 0.9em;"><i class="fa fa-circle text-success" style="font-size: 0.7em;"></i> Online</a>
-        </div>
-      </div>
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
+  <div class="image">
+    <a href="pages/layout/page_proflie_admin.php">
+      <img src="dist_v3/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" style="width: 45px; height: 45px;">
+    </a>
+  </div>
+  <div class="info">
+    <a href="pages/layout/page_proflie_admin.php" class="d-block"><?php echo htmlspecialchars($_SESSION['email'] ?? ''); ?></a>
+    <a href="#" class="d-block" style="color: #c2c7d0; font-size: 0.9em;"><i class="fa fa-circle text-success" style="font-size: 0.7em;"></i> Online</a>
+  </div>
+</div>  
 
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
